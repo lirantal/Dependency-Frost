7cbd4a5fca852c73541892c4918e3287fd360fd9
Eric Smalling <ericsmalling@users.noreply.github.com>
2022-11-16 16:55:02 -0600

    Reset player invincibility after restart and respect right boundary. (#17)
    * Reset player invincibility after restart.
    If player is "invincible" but dies (hits a small, yellow box), then upon game restart, the invicibility remains on until they hit another "Snyk monitor" sprite.
    
    * Stop player from exiting playfield on right border.
    Also adding 'dist' files missing in prior commit.

--- code/main.js
+++ code/main.js
@@ -269,6 +269,8 @@ scene("game", () => {
 
   let helpers = ['Patch-Jumper', 'Protected',  'Protected', 'Mode-filterdevs', 'Mode-filterdevs', 'Protected', 'Mode-filterdevs']
   score = 0
+  playerProtected = false
+  packagesAnimType = 'regular'
 
   let JUMP_FORCE = 705
   const FLOOR_HEIGHT = 48
@@ -329,6 +331,8 @@ scene("game", () => {
   onDraw("player", () => {
     if (player.pos.x <0) {
       player.moveTo(0, player.pos.y)
+    } else if (player.pos.x > width()-player.width) {
+      player.moveTo(width()-player.width, player.pos.y)
     }
   })
 
